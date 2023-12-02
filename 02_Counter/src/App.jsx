df36f17c50454e7b6ec3ccee74aadb91a3b26069
Adarshmundra <adarsh.mundra1305@gmail.com>
2023-12-02 17:22:06 +0530

    first react Project

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+
+function App() {
+  let [counter,setCounter]=useState(0)
+
+  const addValue =()=>{
+    // console.log(`Value Added ${Math.random()}`);
+    // counter+=1
+    if (counter <20){
+    setCounter(counter+1)}
+  }
+
+  const removeValue =()=>{
+    if (counter >0){
+      setCounter(counter-1)}
+  }
+
+
+  return (
+    <>
+      <h1>First React</h1>
+      <h2> counter values {counter}</h2>
+      <br />
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
