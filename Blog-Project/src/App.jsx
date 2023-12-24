434639af493a4b3d85f8b7306cd3558090891621
Adarshmundra <adarsh.mundra1305@gmail.com>
2023-12-24 19:28:38 +0530

    blog project

---
+++ Blog-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import conf from './conf/config';
+
+
+function App() {
+  console.log(conf.appwriteURL);
+  return (
+    <div>
+      <h1>A BLOG WEBSITE</h1>
+    </div>
+  )
+}
+
+export default App
