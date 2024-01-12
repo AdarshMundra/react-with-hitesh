90f5e38f423323086b4a3afad8e8f56b9d27ba27
Adarshmundra <adarsh.mundra1305@gmail.com>
2024-01-12 09:39:59 +0530

    back to blog

--- Blog-Project/src/App.jsx
+++ Blog-Project/src/App.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react'
 import conf from './conf/config';
+
+import { useDispatch } from 'react-redux'
+
+import authService from './appwrite/auth'
+
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
-  console.log(conf.appwriteURL);
-  return (
-    <div>
-      <h1>A BLOG WEBSITE</h1>
+
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+
+
+
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className=' w-full block'>
+        <Header/>
+        <main>
+          TODO:<Outlet />
+        </main>
+        <Footer/>
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default App
