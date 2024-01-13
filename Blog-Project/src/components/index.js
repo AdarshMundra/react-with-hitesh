48f7234a9be50fd91f96b7b0e0c4045e1ce35388
Adarshmundra <adarsh.mundra1305@gmail.com>
2024-01-13 18:39:55 +0530

    blog

--- Blog-Project/src/components/index.js
+++ Blog-Project/src/components/index.js
@@ -1,4 +1,7 @@
+import Container from "./Container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./logo";
 
-export { Header, Footer };
+export { Header, Footer, Logo, LogoutBtn, Container };
