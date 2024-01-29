80ac8d2b3932318beabfb7a0ddf9e5aeee1f93fe
adarshmundra <adarsh.mundra1305@gmail.com>
2024-01-29 23:05:23 +0530

    done

--- Blog-Project/src/components/index.js
+++ Blog-Project/src/components/index.js
@@ -4,7 +4,30 @@ import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import RTE from "./RTE";
+import Singup from "./Signup";
+import Login from "./Login";
 import Button from "./Button";
 import { Select } from "./Select";
 import { Input } from "./Input";
-export { Header, Footer, Logo, LogoutBtn, Container, RTE, Button,Select,Input };
+// import Singup from "../pages/Singup";
+import { PostCard } from "../components/PostCard";
+import PostForm from "./PostForm/PostForm";
+
+import { AuthLayout } from "../components/AuthLayout";
+
+export {
+  Header,
+  PostCard,
+  PostForm,
+  Footer,
+  AuthLayout,
+  Logo,
+  LogoutBtn,
+  Container,
+  RTE,
+  Button,
+  Select,
+  Input,
+  Singup,
+  Login,
+};
---
+++ Blog-Project/src/pages/Singup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Singup as SingupComponent } from "../components";
+
+function Singup() {
+  return (
+    <div>
+      <SingupComponent />
+    </div>
+  );
+}
+
+export default Singup;
